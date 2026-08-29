10def84e89d50a6ed25f5b7b705419d979268d52
xieyuen <xieyuenol@outlook.com>
2026-08-29 20:28:16 +0800

    feat: update type hints in TotalOrdering classes for improved clarity and consistency

--- src/xiepy/utils/total_ordering.py
+++ src/xiepy/utils/total_ordering.py
@@ -9,39 +9,39 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 
-class TotalOrderingLt[ComparableType](ABC):
+class TotalOrderingLt[T](ABC):
     @abstractmethod
     def __eq__(self, other: Any) -> bool:  # self == other
         raise NotImplementedError
 
     @abstractmethod
-    def __lt__(self, other: ComparableType) -> bool:  # self < other
+    def __lt__(self, other: T) -> bool:  # self < other
         raise NotImplementedError
 
-    def __le__(self, other: ComparableType) -> bool:  # self <= other
+    def __le__(self, other: T) -> bool:  # self <= other
         return self < other or self == other
 
-    def __gt__(self, other: ComparableType) -> bool:  # self > other
+    def __gt__(self, other: T) -> bool:  # self > other
         return not (self <= other)
 
-    def __ge__(self, other: ComparableType) -> bool:  # self >= other
+    def __ge__(self, other: T) -> bool:  # self >= other
         return not (self < other)
 
 
-class TotalOrderingGt[ComparableType](ABC):
+class TotalOrderingGt[T](ABC):
     @abstractmethod
     def __eq__(self, other: Any) -> bool:  # self == other
         raise NotImplementedError
 
     @abstractmethod
-    def __gt__(self, other: ComparableType) -> bool:  # self > other
+    def __gt__(self, other: T) -> bool:  # self > other
         raise NotImplementedError
 
-    def __ge__(self, other: ComparableType) -> bool:  # self >= other
+    def __ge__(self, other: T) -> bool:  # self >= other
         return self > other or self == other
 
-    def __lt__(self, other: ComparableType) -> bool:  # self < other
+    def __lt__(self, other: T) -> bool:  # self < other
         return not (self >= other)
 
-    def __le__(self, other: ComparableType) -> bool:  # self <= other
+    def __le__(self, other: T) -> bool:  # self <= other
         return self < other or self == other
